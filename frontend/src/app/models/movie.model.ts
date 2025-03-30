5b4fd58756835ca79d177fc1792048adfb88ec28
ruxeyy04 <ruxepasok356@gmail.com>
2025-03-30 14:16:20 +0800

    Updated movie models

--- frontend/src/app/models/movie.model.ts
+++ frontend/src/app/models/movie.model.ts
@@ -1,3 +1,5 @@
+import { Genre } from '../models/genre.model';
+
 export interface Movie {
     id?: number;
     title: string;
@@ -6,10 +8,14 @@ export interface Movie {
     releaseDate: Date | string | null;
     director: string;
     genre: string[];
+    genres?: Genre[]; // For API response
     duration: number; // in minutes
     rating: number; // 1-10
     posterUrl: string;
     posterUploadFile_url: string | null;
     videoUrl: string;
     videoUploadFile_url: string | null;
+    created_at?: string;
+    updated_at?: string;
 }
+
